03926f5126c0b40a6b3a1a73e41f2d5f1c0fd00f
Xiaoyang Liu <xiaoyang.liu.1@stonybrook.edu>
2022-12-14 15:42:07 -0500

    add comments

--- visualize_corrosion_progression_v1_20210801.py
+++ visualize_corrosion_progression_v1_20210801.py
@@ -17,18 +17,18 @@ from scipy import ndimage
 #files = sorted(glob.glob(folder+'*.tiff'))
 assignvalue = np.arange(20,200,2)
 xy_index_list = []
-img = io.imread('D://Xiaoyin//600C_MLseg_Xiaoyang//quantification_result//img_z100_changepixelvalue.tif')
-material = np.argwhere(img[0]==150)
+img = io.imread('D://Xiaoyin//600C_MLseg_Xiaoyang//quantification_result//img_z100_changepixelvalue.tif') #img is all the insitu data at z=100
+material = np.argwhere(img[0]==150) #150 is the pixel value for corroded Ni-20Cr
 z = img.shape[0]
 y = img.shape[1]
 x = img.shape[2]
 newimg = np.zeros([y,x])
-for m in material:
+for m in material: #make the output image corroded Ni-20Cr pixel value at 150
     newimg[m[0],m[1]] = 150
 for yi in range(y):
     for xi in range(x):
         xy_index_list.append((yi,xi))
-for zi in range(z-1):
+for zi in range(z-1): #compare the consecutive image to check if noncorroded Ni-20Cr beomes pore and assign a pixel value
     for xy in xy_index_list:
         
         if img[zi+1,xy[0],xy[1]] != img[zi,xy[0],xy[1]]:
